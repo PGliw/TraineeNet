eea46f0db375efa73eff9303078ed1083054f103
Piotr Chorościn <titkot96@gmail.com>
2019-12-26 16:42:22 +0100

    Add layouts for login and register

---
+++ app/src/main/java/com/pwr/trainwithme/RegisterFragment.kt
@@ -0,0 +1,36 @@
+package com.pwr.trainwithme
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_register.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_register_fragment_back.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        button_register_fragment_next.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment2)
+        }
+    }
+}
